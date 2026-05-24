aeb94e88446d926bb964350a36bf5c75fdb434f9
bbzorba <bbzorba@hotmail.com>
2026-05-24 16:28:00 +0200

    changes made in stm32 configuration

--- bsp/stm32/stm32f407-st-discovery/rtconfig.h
+++ bsp/stm32/stm32f407-st-discovery/rtconfig.h
@@ -64,6 +64,7 @@
 /* end of rt_strnlen options */
 /* end of klibc options */
 #define RT_NAME_MAX 12
+#define RT_USING_NANO
 #define RT_CPUS_NR 1
 #define RT_ALIGN_SIZE 8
 #define RT_THREAD_PRIORITY_32
@@ -75,6 +76,9 @@
 #define RT_USING_IDLE_HOOK
 #define RT_IDLE_HOOK_LIST_SIZE 4
 #define IDLE_THREAD_STACK_SIZE 256
+#define RT_USING_TIMER_SOFT
+#define RT_TIMER_THREAD_PRIO 4
+#define RT_TIMER_THREAD_STACK_SIZE 512
 
 /* kservice options */
 
@@ -100,7 +104,6 @@
 #define RT_USING_SMALL_MEM_AS_HEAP
 #define RT_USING_HEAP
 /* end of Memory Management */
-#define RT_USING_DEVICE
 #define RT_USING_CONSOLE
 #define RT_CONSOLEBUF_SIZE 128
 #define RT_CONSOLE_DEVICE_NAME "uart1"
@@ -136,58 +139,17 @@
 #define FINSH_USING_DESCRIPTION
 #define FINSH_ARG_MAX 10
 #define FINSH_USING_OPTION_COMPLETION
-
-/* DFS: device virtual file system */
-
-/* end of DFS: device virtual file system */
-
-/* Device Drivers */
-
-#define RT_USING_DEVICE_IPC
-#define RT_UNAMED_PIPE_NUMBER 64
-#define RT_USING_SERIAL
-#define RT_USING_SERIAL_V1
-#define RT_SERIAL_USING_DMA
-#define RT_SERIAL_RB_BUFSZ 64
+#define RT_USING_DFS
+#define RT_USING_FAL
+#define RT_USING_SYSTEM_WORKQUEUE
+#define RT_USING_SDIO
+#define RT_USING_SPI
+#define RT_USING_WIFI
 #define RT_USING_PIN
-/* end of Device Drivers */
-
-/* C/C++ and POSIX layer */
-
-/* ISO-ANSI C layer */
-
-/* Timezone and Daylight Saving Time */
-
-#define RT_LIBC_USING_LIGHT_TZ_DST
-#define RT_LIBC_TZ_DEFAULT_HOUR 8
-#define RT_LIBC_TZ_DEFAULT_MIN 0
-#define RT_LIBC_TZ_DEFAULT_SEC 0
-/* end of Timezone and Daylight Saving Time */
-/* end of ISO-ANSI C layer */
-
-/* POSIX (Portable Operating System Interface) layer */
-
-
-/* Interprocess Communication (IPC) */
-
-
-/* Socket is in the 'Network' category */
-
-/* end of Interprocess Communication (IPC) */
-/* end of POSIX (Portable Operating System Interface) layer */
-/* end of C/C++ and POSIX layer */
-
-/* Network */
-
-/* end of Network */
-
-/* Memory protection */
-
-/* end of Memory protection */
-
-/* Utilities */
-
-/* end of Utilities */
+#define RT_USING_POSIX_FS
+#define RT_USING_POSIX_SOCKET
+#define RT_USING_SAL
+#define RT_USING_LWIP
 
 /* Using USB legacy version */
 
@@ -202,19 +164,133 @@
 
 /* IoT - internet of things */
 
+#define PKG_USING_WIFI_HOST_DRIVER
+
+/* WHD Configuration */
+
+#define WHD_SET_COUNTRY_FROM_HOST
+#define WHD_COUNTRY_CODE "AU"
+#define WHD_COUNTRY_CODE_REVISION 0
+
+/* WHD Thread Configuration */
+
+#define CY_WIFI_WHD_THREAD_PRIORITY 8
+#define CY_WIFI_WHD_THREAD_STACK_SIZE 5120
+/* end of WHD Thread Configuration */
+
+/* WHD Resources Configuration */
+
+#define WHD_RESOURCES_IN_EXTERNAL_STORAGE_FAL
+#define WHD_RESOURCES_FIRMWARE_PART_NAME "whd_firmware"
+#define WHD_RESOURCES_CLM_PART_NAME "whd_clm"
+#define WHD_RESOURCES_NVRAM_PART_NAME "whd_nvram"
+#define WHD_RESOURCES_BLOCK_SIZE 1024
+/* end of WHD Resources Configuration */
+/* end of WHD Configuration */
+
+/* Hardware Configuration */
+
+#define WHD_USING_CHIP_CYW43438
+#define WHD_USING_WIFI5
+
+/* Pin Configuration */
+
+#define CYBSP_USING_PIN_NUMBER
+#define CYBSP_USING_OOB_INTR
+#define CYBSP_REG_ON_PIN -1
+#define CYBSP_HOST_WAKE_IRQ_PIN -1
+#define CYBSP_HOST_WAKE_IRQ_EVENT_FALL
+#define CYBSP_OOB_INTR_PRIORITY 2
+/* end of Pin Configuration */
+/* end of Hardware Configuration */
+
+/* Porting options */
+
+#define WHD_PORTING_BSP
+#define WHD_PORTING_HAL
+#define WHD_PORTING_RTOS
+#define WHD_USE_CUSTOM_MALLOC_IMPL
+/* end of Porting options */
+#define WHD_LOG_LEVEL_ERROR
+#define PKG_USING_WIFI_HOST_DRIVER_LATEST_VERSION
+#define PKG_WIFI_HOST_DRIVER_VER_NUM 0x99999
+#define PKG_USING_ESP_HOSTED
+
+/* ESP-Hosted Configure */
+
+#define ESP_HOSTED_SPI_HOST_INTERFACE
+#define ESP_HOSTED_SLAVE_CHIPSET_ESP32
+#define ESP_HOSTED_MAX_SIMULTANEOUS_SYNC_RPC_REQUESTS 8
+#define ESP_HOSTED_MAX_SIMULTANEOUS_ASYNC_RPC_REQUESTS 8
+#define ESP_HOSTED_RPC_THREAD_PRIORITY 20
+#define ESP_HOSTED_RPC_THREAD_STACK_SIZE 5120
+#define ESP_HOSTED_TRANSPORT_THREAD_PRIORITY 20
+#define ESP_HOSTED_TRANSPORT_THREAD_STACK_SIZE 1024
+#define ESP_HOSTED_TRANSPORT_QUEUE_NUMBER 8
+#define ESP_HOSTED_SPI_DEVICE_NAME "esp-hosted"
+#define ESP_HOSTED_SPI_BUS_NAME "spi1"
+#define ESP_HOSTED_SPI_MODE_3
+#define ESP_HOSTED_SPI_MAX_HZ 30000000
+#define ESP_HOSTED_USING_PIN_NAME
+#define ESP_HOSTED_SPI_CS_PIN_NAME "PA.0"
+#define ESP_HOSTED_DATA_READY_PIN_NAME "PA.0"
+#define ESP_HOSTED_HANDSHAKE_PIN_NAME "PA.0"
+#define ESP_HOSTED_RESET_PIN_NAME "PA.0"
+#define ESP_HOSTED_THREAD_INIT
+#define ESP_HOSTED_INIT_THREAD_STACK_SIZE 2048
+#define ESP_HOSTED_INIT_THREAD_PRIORITY 20
+/* end of ESP-Hosted Configure */
+
+/* Wi-Fi Configure */
+
+#define SOC_WIFI_CSI_SUPPORT
+#define ESP_WIFI_STATIC_RX_BUFFER_NUM 10
+#define ESP_WIFI_DYNAMIC_RX_BUFFER_NUM 32
+#define ESP_WIFI_DYNAMIC_TX_BUFFER
+#define ESP_WIFI_TX_BUFFER_TYPE 1
+#define ESP_WIFI_CACHE_TX_BUFFER_NUM 16
+#define ESP_WIFI_DYNAMIC_TX_BUFFER_NUM 32
+#define ESP_WIFI_AMPDU_TX_ENABLED
+#define ESP_WIFI_TX_BA_WIN 6
+#define ESP_WIFI_AMPDU_RX_ENABLED
+#define ESP_WIFI_RX_BA_WIN 6
+#define ESP_WIFI_SOFTAP_BEACON_MAX_LEN 752
+#define ESP_WIFI_MGMT_SBUF_NUM 32
+#define ESP_WIFI_ENABLE_WPA3_SAE
+#define ESP_WIFI_STA_DISCONNECTED_PM_ENABLE
+#define ESP_WIFI_ESPNOW_MAX_ENCRYPT_NUM 7
+/* end of Wi-Fi Configure */
+#define PKG_USING_ESP_HOSTED_LATEST_VERSION
+#define PKG_ESP_HOSTED_VER_NUM 0x99999
 
 /* Wi-Fi */
 
 /* Marvell WiFi */
 
+#define PKG_USING_WLANMARVELL
+#define PKG_USING_WLANMARVELL_LATEST_VERSION
+#define MARVELLWIFI_USING_STA
+#define MARVELLWIFI_STA_REASSOCIATION
 /* end of Marvell WiFi */
 
 /* Wiced WiFi */
 
 /* end of Wiced WiFi */
+#define PKG_USING_RW007
+#define PKG_USING_RW007_V210
+#define RW007_USING_STM32_DRIVERS
+#define RW007_SPI_MAX_HZ 30000000
+#define RW007_SPI_BUS_NAME "spi2"
+#define RW007_CS_PIN 102
+#define RW007_BOOT0_PIN 175
+#define RW007_BOOT1_PIN 102
+#define RW007_INT_BUSY_PIN 62
+#define RW007_RST_PIN 187
 
 /* CYW43012 WiFi */
 
+#define PKG_USING_WLAN_CYW43012
+#define PKG_USING_WLAN_CYW43012_V001
 /* end of CYW43012 WiFi */
 
 /* BL808 WiFi */
@@ -229,6 +305,16 @@
 /* IoT Cloud */
 
 /* end of IoT Cloud */
+#define PKG_USING_TCPSERVER
+#define TCPSERVER_NAME "tcpserv"
+#define TCPSERVER_STACK_SIZE 4096
+#define TCPSERVER_PRIO 12
+#define TCPSERVER_SOCKET_MAX 512
+#define TCPSERVER_CLI_NUM 5
+#define PKG_USING_TCPSERVER_LATEST_VERSION
+#define PKG_USING_MAVLINK
+#define PKG_USING_MAVLINK_LATEST_VERSION
+#define PKG_USING_MAVLINK_DIALECT_standard
 /* end of IoT - internet of things */
 
 /* security packages */
@@ -259,6 +345,9 @@
 
 /* tools packages */
 
+#define PKG_USING_RVBACKTRACE
+#define PKG_USING_RVBACKTRACE_LATEST_VERSION
+#define PKG_RVBACKTRACE_VER_NUM 0x99999
 /* end of tools packages */
 
 /* system packages */
@@ -280,6 +369,8 @@
 /* Micrium: Micrium software products porting for RT-Thread */
 
 /* end of Micrium: Micrium software products porting for RT-Thread */
+#define PKG_USING_FREERTOS_WRAPPER
+#define PKG_USING_FREERTOS_WRAPPER_LATEST_VERSION
 /* end of system packages */
 
 /* peripheral libraries and drivers */
@@ -333,10 +424,16 @@
 /* FT32 HAL & SDK Drivers */
 
 /* end of FT32 HAL & SDK Drivers */
+
+/* NOVOSNS Drivers */
+
+/* end of NOVOSNS Drivers */
 /* end of HAL & SDK Drivers */
 
 /* sensors drivers */
 
+#define PKG_USING_BME680
+#define PKG_USING_BME680_LATEST_VERSION
 /* end of sensors drivers */
 
 /* touch drivers */
@@ -414,21 +511,6 @@
 /* end of RT-Thread online packages */
 #define SOC_FAMILY_STM32
 #define SOC_SERIES_STM32F4
-
-/* Hardware Drivers Config */
-
-/* Onboard Peripheral Drivers */
-
-/* On-chip Peripheral Drivers */
-
-#define BSP_USING_GPIO
-#define BSP_USING_UART
-#define BSP_STM32_UART_V1_TX_TIMEOUT 2000
-#define BSP_USING_UART1
-/* end of On-chip Peripheral Drivers */
-
-/* Board extended module Drivers */
-
-/* end of Hardware Drivers Config */
+#define BSP_USING_SPI
 
 #endif
